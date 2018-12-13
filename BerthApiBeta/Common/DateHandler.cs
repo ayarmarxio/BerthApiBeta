db48d0a00ef3eb4f6556c7c0dbdf428171ccbae4
ayarmarxio <ayarmarxio@gmail.com>
2018-12-13 12:35:45 +0100

    Data Handler edit

--- BerthApiBeta/Common/DateHandler.cs
+++ BerthApiBeta/Common/DateHandler.cs
@@ -11,21 +11,7 @@ namespace BerthApiBeta.Common
         {
 
         }
-
-        //public string ConvertToSqlStringDate(string time)
-        //{
-        //    DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
-        //    dateTime = dateTime.AddSeconds(unixTime).ToLocalTime();
-        //    string date = dateTime.ToShortDateString();
-
-        //    string[] dateSplitted = time.Split('/');
-        //    string month = dateSplitted[0];
-        //    string day = dateSplitted[1];
-        //    string year = dateSplitted[2];
-
-        //    string newDateString = "'" + month + "-" + day + "-" + year + "'";
-        //    return newDateString;
-        //}
+        
 
         public string ConvertToSqlStringDate(double unixTime)
         {
